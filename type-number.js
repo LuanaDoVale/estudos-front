74d9de3a317ec46ddf3f86693af1e9093a8f4aca
Luana do Vale <luana.dovalegomes@gmail.com>
2022-10-11 17:27:15 -0300

    exemplos da aula 1 do curso de JavaScript: tipos, variáveis e funções

---
+++ type-number.js
@@ -0,0 +1,32 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 4;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
+
+//template string ou template literal
+
+// transformação / conversão do código Unicode em caractere.
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const alura ="Alura";
+console.log(alura * primeiroNumero);
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Ola, mundo!";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Luana"
+
+//concatenacao (+)
+
+console.log(citacao + meuNome)
